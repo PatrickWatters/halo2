9a587dd4e0c906c4f5d36e9d1916385a61e065e0
Patrick Watters <>
2023-08-15 15:04:26 -0700

    gpu and sha236 clen - before major changes

--- halo2_proofs/src/lib.rs
+++ halo2_proofs/src/lib.rs
@@ -30,9 +30,19 @@ pub mod circuit;
 pub use halo2curves;
 mod multicore;
 pub mod plonk;
+/// Gpu fft
+#[cfg(feature = "gpu")]
+pub mod gpu;
+/// Gpu worker thread
+pub mod worker {
+    pub use super::multicore::*;
+}
 pub mod poly;
 pub mod transcript;
 
 pub mod dev;
 mod helpers;
 pub use helpers::SerdeFormat;
+#[cfg(feature = "gpu")]
+#[macro_use]
+extern crate lazy_static;
